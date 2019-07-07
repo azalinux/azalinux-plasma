7ed45a0b89b0033f3c50a21e7105852610db93e9
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-07 14:58:34 +1000

    Update post_reboot_apps.sh

--- post_reboot_apps.sh
+++ post_reboot_apps.sh
@@ -4,9 +4,16 @@ set -e
 yay -S pamac-aur
 yay -S vivaldi
 
-pacman -S firefox-developer-edition
-pacman -S qbittorrent
-pacman -S neofetch
-pacman -S chromium
-pacman -S libreoffice-fresh
+sudo pacman -S --noconfirm --needed xdg-user-dirs
+sudo pacman -S --noconfirm --needed wget
+sudo pacman -S --noconfirm --needed net-tools
+sudo pacman -S --noconfirm --needed htop
+sudo pacman -S --noconfirm --needed gtop
+sudo pacman -S --noconfirm --needed gparted
+
+sudo pacman -S firefox-developer-edition
+sudo pacman -S qbittorrent
+sudo pacman -S neofetch
+sudo pacman -S chromium
+sudo pacman -S libreoffice-fresh
 
