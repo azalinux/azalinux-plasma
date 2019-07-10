3e1dda7ca129c7523fd5f9f496b8bc42fc1a703c
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-10 11:58:52 +1000

    Update reflector.sh

--- reflector.sh
+++ reflector.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -S reflector
+sudo pacman -S --noconfirm reflector
 sudo reflector -c Australia --save /etc/pacman.d/mirrorlist
-sudo pacman -Syuu
+sudo pacman -Syuu --noconfirm
