41450ab14df2b13f67a7ed7aa9b93bca428d5da1
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-11 11:43:06 +1000

    Update samba_install.sh

--- samba_install.sh
+++ samba_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+sudo pacman -S samba
 sudo mkdir /var/lib/samba/usershares
-groupadd -r sambashare
+sudo groupadd -r sambashare
 sudo chown root:sambashare /var/lib/samba/usershares
 chmod 1770 /var/lib/samba/usershares
 gpasswd sambashare -a aza
 
 sudo wget "https://raw.githubusercontent.com/azalinux/azalinux/master/smb.conf" -O /etc/samba/smb.conf
+
+sudo systemctl enable smb nmb
+sudo systemctl start smb nmb
