24b4d6a762b2b02b9178d811267dedd9602a4252
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-12 12:08:59 +1000

    Update post_install_apps.sh

--- post_install_apps.sh
+++ post_install_apps.sh
@@ -10,6 +10,7 @@ sh cores.sh
 
 sudo pacman -S --noconfirm --needed xorg
 sudo pacman -S --noconfirm --needed openssh
+sudo systemctl enable openssh
 sudo pacman -S --noconfirm --needed dhcpcd
 sudo pacman -S --noconfirm --needed plasma-meta
 #pacman -S --noconfirm --needed kde-applications-meta
