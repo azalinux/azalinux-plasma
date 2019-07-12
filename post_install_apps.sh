52e7d34605bfa2843c7f8832d58e53e506fa963f
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-12 12:02:00 +1000

    Update post_install_apps.sh

--- post_install_apps.sh
+++ post_install_apps.sh
@@ -34,17 +34,6 @@ sudo pacman -S --noconfirm --needed kwrite
 sudo pacman -S --noconfirm --needed kde-gtk-config
 sudo pacman -S --noconfirm --needed grub-customizer 
 sudo pacman -S --noconfirm --needed wget
-
-sh sddm.sh
-
-sh trizen.sh
-
-trizen -S --noconfirm --needed --noedit pamac-aur
-trizen -S --noconfirm --needed --noedit vivaldi
-trizen -S --noconfirm --needed --noedit vivaldi-codecs-ffmpeg-extra-bin
-trizen -S --noconfirm --needed --noedit realvnc-vnc-server
-trizen -S --noconfirm --needed --noedit realvnc-vnc-viewer
-
 sudo pacman -S --noconfirm --needed net-tools
 sudo pacman -S --noconfirm --needed htop
 sudo pacman -S --noconfirm --needed gtop
@@ -56,9 +45,20 @@ sudo pacman -S --noconfirm --needed geany
 sudo pacman -S --noconfirm --needed meld
 sudo pacman -S --noconfirm --needed catfish
 sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller
-
 sudo pacman -S --noconfirm --needed firefox-developer-edition
 sudo pacman -S --noconfirm --needed qbittorrent
 sudo pacman -S --noconfirm --needed neofetch
 sudo pacman -S --noconfirm --needed chromium
 sudo pacman -S --noconfirm --needed libreoffice-fresh
+
+sh sddm.sh
+
+sh trizen.sh
+
+trizen -S --noconfirm --needed --noedit pamac-aur
+trizen -S --noconfirm --needed --noedit vivaldi
+trizen -S --noconfirm --needed --noedit vivaldi-codecs-ffmpeg-extra-bin
+trizen -S --noconfirm --needed --noedit realvnc-vnc-server
+trizen -S --noconfirm --needed --noedit realvnc-vnc-viewer
+
+
