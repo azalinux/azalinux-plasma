c06dc14b33b6245ceaa6ebecdfd1f11c04b1d25b
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-12 20:40:01 +1000

    Update sddm.sh

--- sddm.sh
+++ sddm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 sudo mkdir /etc/sddm.conf.d
 sudo wget "https://raw.githubusercontent.com/azalinux/azalinux/master/autologin.conf" -O /etc/sddm.conf.d/autologin.conf
 sudo gpasswd -a aza sddm
