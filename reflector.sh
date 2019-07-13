e84a8abbc106a385104ac9c945458392b4b0eeb6
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-13 17:27:52 +1000

    Update reflector.sh

--- reflector.sh
+++ reflector.sh
@@ -3,5 +3,5 @@ set -e
 set -u
 
 pacman -S --noconfirm reflector
-reflector -c Australia -p http -p https --save /etc/pacman.d/mirrorlist
+reflector --latest 200 -c Australia -p http -p https --save /etc/pacman.d/mirrorlist
 
