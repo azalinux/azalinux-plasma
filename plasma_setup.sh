bcae904959743818bfbf2daa0d8838734d5568f6
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-20 16:23:30 +1000

    Update plasma_setup.sh

--- plasma_setup.sh
+++ plasma_setup.sh
@@ -13,6 +13,12 @@ sudo systemctl enable sshd
 sudo pacman -S --noconfirm --needed dhcpcd
 sudo pacman -S --noconfirm --needed plasma-meta
 #pacman -S --noconfirm --needed kde-applications-meta
+sudo pacman -S pulseaudio --noconfirm --needed
+sudo pacman -S pulseaudio-alsa --noconfirm --needed
+sudo pacman -S pavucontrol  --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-lib alsa-firmware --noconfirm --needed
+sudo pacman -S gstreamer --noconfirm --needed
+sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly --noconfirm --needed
 sudo pacman -S --noconfirm --needed kdeaccessibility-meta 
 sudo pacman -S --noconfirm --needed kdeadmin-meta
 sudo pacman -S --noconfirm --needed kdeedu-meta
