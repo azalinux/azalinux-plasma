49e9b12afa851b090e7b761167d16d9e380bd60c
Azalinux <52433182+azalinux@users.noreply.github.com>
2021-05-04 19:50:21 +1000

    Update sddm.sh

--- sddm.sh
+++ sddm.sh
@@ -3,6 +3,6 @@ set -e
 set -u
 
 sudo mkdir /etc/sddm.conf.d
-sudo wget "https://raw.githubusercontent.com/azalinux/azalinux/master/autologin.conf" -O /etc/sddm.conf.d/autologin.conf
+sudo wget "https://raw.githubusercontent.com/azalinux/azalinux-plasma/master/autologin.conf" -O /etc/sddm.conf.d/autologin.conf
 sudo gpasswd -a aza sddm
 sudo systemctl enable sddm
